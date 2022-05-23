2de713fac0d19ad971bb4045699b59b485e4aa9d
ASLManasa <aslmanasa3@gmail.com>
2022-05-23 18:07:48 +0530

    update keys

---
+++ Firebase/src/index.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+    apiKey: "AIzaSyALMR2nuw0WmAN6qzhznxGZE5J0Qd0jjIg",
+    authDomain: "practice-4a2ce.firebaseapp.com",
+    projectId: "practice-4a2ce",
+    storageBucket: "practice-4a2ce.appspot.com",
+    messagingSenderId: "735232810379",
+    appId: "1:735232810379:web:db9bd5c2d8fcddfd74e38e"
+  };
+
+ initializeApp(firebaseConfig)
